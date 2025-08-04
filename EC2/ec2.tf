ed1e6bcc9c208f0415761919ee1974076fcf45b1
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-08-04 10:16:16 +0530

    added files

---
+++ EC2/ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_alltraffic" {
+    name        = "allow_all"
+    description = "Allow all inbound traffic and all outbound traffic"
+
+    ingress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+}
