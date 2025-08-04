0c9703da1e91bd60dd2ec768b410ee9fa55c53cb
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-08-04 11:17:08 +0530

    .

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,3 +1,13 @@
+resource "aws_instance" "example" {
+  ami           = "ami-09c813fb71547fc4f" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.allow_alltraffic.name]
+
+  tags = {
+    Name = "SampleInstance"
+  }
+}
+
 resource "aws_security_group" "allow_alltraffic" {
     name        = "allow_all"
     description = "Allow all inbound traffic and all outbound traffic"
